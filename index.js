b244cc4c420dcdb277a5f56f2b898191b64c3c63
Todd McNeill <todd-mcneill@pluralsight.com>
2021-05-11 10:14:08 -0600

    Test setup

---
+++ index.js
@@ -0,0 +1,7 @@
+function add() {
+
+}
+
+module.exports = {
+  add
+}
---
+++ index.test.js
@@ -0,0 +1,11 @@
+const { add } = require('./index')
+const { assert } = require('chai')
+
+describe('string calculator', () => {
+  describe('add', () => {
+    it('placeholder', () => {
+      add()
+      assert(true)
+    })
+  })
+})
